e02410ad01d29cf8eb289d8cba1d3c4fe2410de1
军舰 <wang-junjian@qq.com>
2023-11-05 19:42:48 +0800

    同步9年级最新版Unit4-6的课本；从文件中读取所有的单词。

--- utils/convert_format.py
+++ utils/convert_format.py
@@ -1,3 +1,6 @@
+import os
+
+
 def read_words(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -21,5 +24,28 @@ def read_words(filename):
 
     return words
 
-words = read_words('english/9/Unit14.txt')
-print(words)
+
+if __name__ == '__main__':
+    unit_template = "    '{unit_id}' : {unit_words},\n"
+
+    unit_id_words = []
+
+    def sort_key(filename):
+        # 去掉文件名的扩展名和前缀，然后转换为整数
+        return int(filename.replace('Unit', '').replace('.txt', ''))
+
+    root_dir = 'english/9'
+    for filename in sorted(os.listdir(root_dir), key=sort_key):
+        if not filename.endswith('.txt'):
+            continue
+
+        id = filename[4:-4]
+        words = read_words(os.path.join(root_dir, filename))
+        id_words = unit_template.format(unit_id=id, unit_words=words)
+        unit_id_words.append(id_words)
+
+    result_template = "{\n%s}"
+    result = result_template % ''.join(unit_id_words)
+
+    with open('english/results/9.txt', 'w') as file:
+        file.write(result)
